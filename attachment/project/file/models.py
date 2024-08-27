f434377e4b81a590bbf509bdbf0b58e07631c9e2
Misbah Noor <66489320+Mishaa931@users.noreply.github.com>
2024-08-27 14:51:04 +0500

    Delete attachment/project/file directory

--- attachment/project/file/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-
-class FileUpload(models.Model):
-    csv_file_upload = models.FileField(upload_to="csvfile")
--- attachment/project/file/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers 
-from .models import *
-
-class FileUploadSerializer(serializers.ModelSerializer):
-    class Meta:
-        models=FileUpload
-        fileds='__all__'
